74c01c72ee7edc7c59a8678232fb8848e92dc1bf
invin_lynx <nikhilbutola33@gmail.com>
2019-03-29 00:46:51 +0530

    Drowsiness Detection Prototype

---
+++ drow_detection.py
@@ -0,0 +1,80 @@
+from scipy.spatial import distance as dist
+from imutils.video import VideoStream
+from imutils import face_utils
+import numpy as np
+import playsound
+import imutils
+import time
+import dlib
+import cv2
+
+
+def sound_alarm(path):
+    playsound.playsound(path)
+
+
+def eye_aspect_ratio(eye):
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+    C = dist.euclidean(eye[0], eye[3])
+    ear = (A + B) / (2.0 * C)
+    return ear
+
+
+EYE_AR_THRESH = 0.3
+EYE_AR_CONSEC_FRAMES =50
+
+COUNTER = 0
+ALARM_ON = False
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+vs = VideoStream(src=0).start()
+print(vs)
+time.sleep(1.0)
+
+while True:
+    frame = vs.read()
+    frame = imutils.resize(frame, width=819)
+    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    print(frame)
+    print(gray)
+    
+    rects = detector(gray, 1)
+    for rect in rects:
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
+        ear = (leftEAR + rightEAR) / 2.0
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+        if ear < EYE_AR_THRESH:
+            COUNTER += 1
+            if COUNTER >= EYE_AR_CONSEC_FRAMES:
+                if not ALARM_ON:
+                    ALARM_ON = True
+                cv2.putText(frame, "DROWSINESS ALERT! The System is demoted", (10, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        else:
+            COUNTER = 0
+            ALARM_ON = False
+        cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+vs.stop()
